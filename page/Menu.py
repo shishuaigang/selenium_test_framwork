1e03dbbd4735d97292bb1751eda25fc84207e617
shishuaigang <shishuaigang@163.com>
2018-01-16 14:06:42 +0800

    add a page and one testcase for unittest

---
+++ page/Menu.py
@@ -0,0 +1,3 @@
+from selenium.webdriver.common.by import By
+
+Workbill_Config = (By.LINK_TEXT,'作业票配置')
--- testcase/test_intranet.py
+++ testcase/test_intranet.py
@@ -10,27 +10,37 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 from utils.readYaml import ReadYaml
 from utils import location
-from page import login
+from page import login, Menu
 
 
 class TestIntranet(unittest.TestCase):
     url = ReadYaml(location.CONFIG_FILE).yaml_data()['URL']
 
-    def setUp(self):
-        self.FF = webdriver.Chrome()
-        self.FF.get(self.url)
+    @classmethod
+    def setUpClass(cls):
+        option = webdriver.ChromeOptions()
+        option.add_argument('disable-infobars')
+
+        cls.FF = webdriver.Chrome(executable_path="C:\Selenium\chromeDriver\chromedriver.exe", chrome_options=option)
+        cls.FF.get(cls.url)
+        cls.FF.maximize_window()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.FF.quit()
 
     def testLogin(self):
         self.FF.find_element(*login.phone_number).clear()
         self.FF.find_element(*login.phone_number).send_keys('15102100358')
         self.FF.find_element(*login.password).clear()
-        WebDriverWait(self.FF, 20, 0.5).until(EC.visibility_of_element_located(login.password))
         self.FF.find_element(*login.password).send_keys('123456')
         self.FF.find_element(*login.login_btn).click()
-        time.sleep(5)
+        self.assertNotEqual(EC.visibility_of_element_located('我的工作面板'), False)
 
-    def tearDown(self):
-        self.FF.quit()
+    def testSearchWorkbill(self):
+        WebDriverWait(self.FF, 20, 0.5).until(EC.visibility_of_element_located(Menu.Workbill_Config))
+        self.FF.find_element(*Menu.Workbill_Config).click()
+        time.sleep(20)
 
 
 if __name__ == '__main__':
@@ -39,6 +49,7 @@ if __name__ == '__main__':
     print(location.REPORT_PATH)
     f = open(os.path.join(location.REPORT_PATH, 'report_' + now + '.html'), 'w', encoding='utf-8')
     runner = HTMLTestRunner.HTMLTestRunner(stream=f, title=u'测试报告', description='')
-    test_unit.addTest(TestIntranet('testSearch'))
+    test_unit.addTest(TestIntranet('testLogin'))
+    test_unit.addTest(TestIntranet('testSearchWorkbill'))
     runner.run(test_unit)
     f.close()
