c9f05dadb1cd430302e4a58f748a8b9195fdf347
shishuaigang <shishuaigang@163.com>
2018-01-25 10:58:40 +0800

    add commonActions path into location.py

--- utils/location.py
+++ utils/location.py
@@ -10,11 +10,12 @@ import os
 # CurrentDir = os.path.dirname(os.path.abspath(__file__))  # 当前文件的目录
 ROOT_DIR = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
 
+COMMONACTIONS_PATH = os.path.join(ROOT_DIR, 'commonActions')
 CONFIG_PATH = os.path.join(ROOT_DIR, 'config')
 CONFIG_FILE = os.path.join(ROOT_DIR, 'config', 'config.yaml')
 DATA_PATH = os.path.join(ROOT_DIR, 'data')
 DRIVER_PATH = os.path.join(ROOT_DIR, 'drivers')
-LOG_PATH = os.path.join(ROOT_DIR, 'page')
+PAGE_PATH = os.path.join(ROOT_DIR, 'page')
 REPORT_PATH = os.path.join(ROOT_DIR, 'report')
 TESTCASE_PATH = os.path.join(ROOT_DIR, 'testcase')
 UTILS_PATH = os.path.join(ROOT_DIR, 'utils')
